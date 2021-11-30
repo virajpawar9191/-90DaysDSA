c32635e12017c242aaa8e50bc9fd94a51906c0c1
Viraj Pawar <virajpawar9394@gmail.com>
2021-11-30 21:33:11 +0530

    Add files via upload
    First Commit

---
+++ First.java
@@ -0,0 +1,34 @@
+package com.DsaBegin;
+
+public class First {
+
+    public static void main(String[] args) {
+	/*
+         write your code here
+         input output
+         println
+         Scanner class is ud=sed for taking input using object of that class
+         eg. Scanner input = new Scanner(System.in);
+         Data types
+         Primitive : int float double char long boolean
+         Non-Primitive : String
+
+         // Type Conversion and Type Casting
+         A. Type Conversion : Conversion of data from one type to another Data type
+                E.g.
+                float b = 100.56f;
+                int a = (int)(b);
+                will return 100;
+         B. Type Casting : when type conversion is happens automatically is called type casting
+                1. Destination type should be Compatible
+                and destination type should be greater than source type
+
+        */
+//        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        for (int i: arr
+//             ) {
+//            System.out.print(i + " ");
+        }
+
+    }
+
